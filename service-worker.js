81d415b2d6f13e5113b3849d8673d77a22353a65
agamsantika91 <agamsantika91@gmail.com>
2020-10-05 20:43:39 +0700

    subs pwa dicoding

---
+++ service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = "restoku";
+var urlsToCache = [
+  "/",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+  "/manifest.json",
+  "/index.html",
+  "/nav.html",
+  "/pages/home.html",
+  "/pages/profile.html",
+  "/pages/menu.html",
+  "/pages/foodcourt.html",
+  "/css/materialize.min.css",
+  "/css/style.css",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+  "/asset/header.png",
+  "/asset/menu1.jpg",
+  "/asset/menu2.jpg",
+  "/asset/menu3.jpg",
+  "/asset/menu4.jpg",
+  "/asset/menu5.jpg",
+  "/asset/menu7.jpg",
+  "/asset/foodcourt1.jpg",
+  "/asset/foodcourt2.jpg",
+
+];
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+          if (response) {
+            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+            return response;
+          }
+   
+          console.log(
+            "ServiceWorker: Memuat aset dari server: ",
+            event.request.url
+          );
+          return fetch(event.request);
+        })
+    );
+  });
+
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+              console.log("ServiceWorker: cache " + cacheName + " dihapus");
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  }); 
