88c18a1fe4d7e60cdcae603ccedd2f6e81581e0a
agamsantika91 <agamsantika91@gmail.com>
2020-10-06 20:41:20 +0700

    service-worker.js edit

--- service-worker.js
+++ service-worker.js
@@ -23,17 +23,18 @@ var urlsToCache = [
   "/asset/menu7.jpg",
   "/asset/foodcourt1.jpg",
   "/asset/foodcourt2.jpg",
-
+  "/css/materialize.css"
 ];
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
 
-self.addEventListener("fetch", function(event) {
+  self.addEventListener("install", function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  self.addEventListener("fetch", function(event) {
     event.respondWith(
       caches
         .match(event.request, { cacheName: CACHE_NAME })
@@ -42,7 +43,7 @@ self.addEventListener("fetch", function(event) {
             console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
             return response;
           }
-   
+  
           console.log(
             "ServiceWorker: Memuat aset dari server: ",
             event.request.url
@@ -51,18 +52,3 @@ self.addEventListener("fetch", function(event) {
         })
     );
   });
-
-  self.addEventListener("activate", function(event) {
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheName != CACHE_NAME) {
-              console.log("ServiceWorker: cache " + cacheName + " dihapus");
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  }); 
